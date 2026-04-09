092472a5128ba622a52d9ba9bf974d17ed765854
Christopher Hills <oneswitchguy@icloud.com>
2026-04-09 16:22:13 +1000

    Minor UI changes to controls sheet Build number increments Returning to magenta joystick state when motion data is unavailable New app name

---
+++ CHDroneTests/JoystickControlsModelTests.swift
@@ -0,0 +1,63 @@
+//
+//  JoystickControlsModelTests.swift
+//  CHDroneTests
+//
+//  Created by Codex on 13/10/2025.
+//
+
+import XCTest
+
+@testable import CH_Drone
+
+final class JoystickControlsModelTests: XCTestCase {
+
+    private var defaults: UserDefaults!
+    private var model: JoystickControlsModel!
+
+    override func setUp() async throws {
+        defaults = UserDefaults.makeTestDefaults()
+        model = JoystickControlsModel(userDefaults: defaults)
+    }
+
+    func testHeadphoneDisconnectInterruptsAirPodsControl() {
+        model.joystickMoved(to: CGPoint(x: 0.4, y: -0.6), source: .airPods)
+
+        model.handleHeadphoneConnectionChanged(isConnected: false)
+
+        XCTAssertEqual(model.uiState, .idle)
+        XCTAssertFalse(model.airPodsMotionState.isControlling)
+        XCTAssertFalse(model.airPodsMotionState.isAvailable)
+    }
+
+    func testHeadphoneDisconnectCancelsAirPodsCountdown() {
+        model.handleHeadphoneConnectionChanged(isConnected: true)
+        model.engageAirPodsControl()
+
+        XCTAssertTrue(model.airPodsMotionState.isCountingDown)
+
+        model.handleHeadphoneConnectionChanged(isConnected: false)
+
+        XCTAssertNil(model.airPodsMotionState.countdownValue)
+        XCTAssertFalse(model.airPodsMotionState.isControlling)
+        XCTAssertEqual(model.uiState, .idle)
+    }
+
+    func testHeadphoneMotionIgnoredAfterDisconnectUntilReengaged() {
+        model.joystickMoved(to: CGPoint(x: 0.1, y: 0.2), source: .airPods)
+        model.handleHeadphoneConnectionChanged(isConnected: false)
+
+        model.handleHeadphoneMotion(CGPoint(x: 0.8, y: 0.8))
+
+        XCTAssertEqual(model.uiState, .idle)
+    }
+
+    func testScreenControlStillRequiresFreshTouchAfterDisconnect() {
+        model.joystickMoved(to: CGPoint(x: 0.25, y: 0.25), source: .airPods)
+        model.handleHeadphoneConnectionChanged(isConnected: false)
+
+        model.joystickMoved(to: CGPoint(x: -0.5, y: 0.75), source: .screen)
+
+        XCTAssertEqual(model.uiState, .active(location: CGPoint(x: -0.5, y: 0.75), source: .screen))
+        XCTAssertFalse(model.airPodsMotionState.isControlling)
+    }
+}
